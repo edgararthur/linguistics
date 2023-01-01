02bcc0a893345bd71d35a3f639d8ca362f83d1f3
edgararthur <gilfoyle.arthur@protonmail.com>
2023-01-01 07:07:10 +0000

    recieve email message from client

--- js/email.js
+++ js/email.js
@@ -1,3 +1,5 @@
+import { hideClientId, hideApiId, hideFromAddress } from '../config.js';
+
 const bar = document.querySelector('.fa-bars');
 const close = document.querySelector('.fa-close')
 
@@ -17,10 +19,11 @@ document.querySelector('a').addEventListener('click', e => {
     console.log('hello');
 })
 
-const client_id = ""
-const api_key = ""
-
 const form = document.querySelector('form');
+
+const clientId = hideClientId;
+const apiId = hideApiId;
+const fromAddress = hideFromAddress
 
 const scopes = ['https://www.googleapis.com/auth/gmail.send']
 
@@ -45,50 +48,73 @@ const handleSignIn = () => {
 //   "restrictToContacts": false
 // }
 
-const initClient = () => {
-    gapi.client.init({
-        apiKey: api_key,
-        clientId: client_id,
-        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'],
-        scope: scopes.join(' '),
-    }).then(() => {
-        // Listen for sign-in state changes
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-        // Handle the initial sign-in state
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    });
-}
-
 // Function to send an email using the Gmail API
-function sendEmail(userName, userEmail, userMessage) {
-    const message = {
-        to: `${fromAddress}`,
-        subject: `Message from LAG Website. ${userName}`,
-        message: `${userMessage}`,
-    };
-
-    const fromAddress = 'bysschearthur123@gmail.com';
-  
-    gapi.client.gmail.users.messages.send({
-        userId: 'me',
-        resource: {
+// Function to send an email using the Gmail API
+async function sendEmail(userName, userEmail, userMessage) {
+    try {
+        // Ensure the Gmail API is loaded and initialized
+        await initClient();
+    
+        const message = {
+            to: hideFromAddress,
+            subject: `Message from LAG Website. ${userName}`,
+            message: `${userMessage}`,
+        };
+    
+        // Send the email
+        const response = await gapi.client.gmail.users.messages.send({
+            userId: 'me',
+            resource: {
             raw: btoa(createEmail(message, userEmail)),
-        },
-    }).then((response) => {
+            },
+        });
+    
         console.log(response);
-    });
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
 }
   
+
 // Function to create a properly formatted email message
-const createEmail = (message, fromAddress) => {
+function createEmail(message, fromAddress) {
     const emailContent = [
-        `From: ${fromAddress}`
-        `To: ${message.to}`,
-        'Content-Type: text/plain; charset="UTF-8"',
-        'MIME-Version: 1.0',
-        `Subject: ${message.subject}`,
-        '',
-        message.message,
+      `From: ${fromAddress}`,
+      `To: ${message.to}`,
+      'Content-Type: text/plain; charset="UTF-8"',
+      'MIME-Version: 1.0',
+      `Subject: ${message.subject}`,
+      '',
+      message.message,
     ].join('\n');
     return emailContent;
+}
+  
+  // Initialize the Gmail API client
+async function initClient() {
+    try {
+      // Load the Google API Client Library
+      await new Promise((resolve) => gapi.load('client:auth2', resolve));
+  
+      // Initialize the client library and set up the API key, clientId, etc.
+      await gapi.client.init({
+        apiKey: hideApiId,
+        clientId: hideClientId,
+        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'],
+        scope: 'https://www.googleapis.com/auth/gmail.send',
+      });
+  
+      // Listen for sign-in state changes
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+  
+      // Handle the initial sign-in state
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    } catch (error) {
+      console.error('Error initializing Gmail API client:', error);
+    }
+}
+  
+  // Function to handle sign-in status changes
+function updateSigninStatus(isSignedIn) {
+    // Handle sign-in status changes if needed
 }
