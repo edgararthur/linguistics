1669e5787d6e527564e86a176f7b8cb5d9184273
edgararthur <gilfoyle.arthur@protonmail.com>
2023-10-24 08:32:18 +0000

    refactor news animation timeout after making api request

--- js/index.js
+++ js/index.js
@@ -1,83 +1,102 @@
+// Variables
 const members = document.querySelector('.members');
-const events = document.querySelector('.events')
-const option = document.createElement("option");
-
-option.classList.add("custom-option");
+const events = document.querySelector('.events');
 const customCountryCodeSelect = document.getElementById("country-code");
-
-events.addEventListener('click', e => {
-    document.querySelector(".event__card").classList.toggle('visibility')
-})
-
-members.addEventListener('click', (e) => {
-    document.querySelector(".membership__card").classList.toggle('visibility')
-})
-
-fetch('https://restcountries.com/v3.1/all')
-.then(response => response.json())
-.then(data => {
-    data.forEach(country => {
-        option.innerHTML = `<p style="{width: 100%}">+${country}</p>`
-    })
-});
-
-// script.js
 const imageContainer = document.querySelector('.desc');
-const imageUrls = [
-    'images/lag.JPG',
-    'images/LAG_2023.jpg',
-];
-
+const news = document.querySelector('.news__list');
+const links = document.querySelectorAll('a');
+const bar = document.querySelector('.fa-bars');
+const closeBtn = document.querySelector('.fa-close');
+const imageUrls = ['images/lag.JPG', 'images/LAG_2023.jpg'];
 let currentIndex = 0;
-let backgroundPosition = 0;
+
+// Functions
+function toggleVisibility(elementSelector) {
+	const element = document.querySelector(elementSelector);
+	if (element) {
+		element.classList.toggle('visibility');
+	}
+}
 
 function changeBackgroundImage() {
-    currentIndex = (currentIndex + 1) % imageUrls.length;
-    imageContainer.style.backgroundImage = `url('${imageUrls[currentIndex]}')`;
+	currentIndex = (currentIndex + 1) % imageUrls.length;
+	imageContainer.style.backgroundImage = `url('${imageUrls[currentIndex]}')`;
+	let backgroundPosition = 100;
 
-    backgroundPosition = 100;
-    imageContainer.style.backgroundPosition = `0% ${backgroundPosition}%`;
-
-    const interval = setInterval(() => {
-        backgroundPosition--;
-        imageContainer.style.backgroundPosition = `0% ${backgroundPosition}%`;
-        if (backgroundPosition === 0) {
-            clearInterval(interval);
-        }
-    }, 50);
-
-    // imageContainer.classList.add("")
+	const interval = setInterval(() => {
+		backgroundPosition--;
+		imageContainer.style.backgroundPosition = `0% ${backgroundPosition}%`;
+		if (backgroundPosition === 0) {
+		clearInterval(interval);
+		}
+	}, 50);
 }
+
+function createOptionElement(country) {
+	const option = document.createElement("option");
+	option.classList.add("custom-option");
+	option.innerHTML = `<p style="{width: 100%}">+${country}</p>`;
+	return option;
+}
+
+// Event Listeners
+events.addEventListener('click', () => {
+  	toggleVisibility(".event__card");
+});
+
+members.addEventListener('click', () => {
+  	toggleVisibility(".membership__card");
+});
+
+links.forEach(link => {
+	link.addEventListener('click', () => {
+		console.log("hello world");
+	});
+});
+
+bar.addEventListener('click', () => {
+	document.querySelector('.links').classList.add('links__visibility');
+	bar.style.display = 'none';
+	closeBtn.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+	document.querySelector('.links').classList.remove('links__visibility');
+	bar.style.display = 'block';
+	closeBtn.style.display = 'none';
+});
 
 // Initial background image
 changeBackgroundImage();
 setInterval(changeBackgroundImage, 8000);
 
-const links = document.querySelector('a');
-links.addEventListener('click', (e) => {
-    console.log("hello world")
-})
+// Fetch API for News
+fetch('http://127.0.0.1:8000/api/news/')
+	.then(response => response.json())
+	.then(data => {
+		data.forEach(element => {
+			console.log(element)
 
-const bar = document.querySelector('.fa-bars');
-const closeBtn = document.querySelector('.fa-close')
+			const newItem = document.createElement('div');
+			newItem.classList.add('news__item');
+			const image = `<img src="${element.image}" alt=""></img>`
+			const h3 = `<h3>${element.content}</h3>`
 
-bar.addEventListener('click', (e) => {
-    document.querySelector('.links').classList.add('links__visibility');
-    bar.style.display = 'none';
-    closeBtn.style.display = 'block'
-})
+			setTimeout(() => {
+				newItem.innerHTML = image + h3
+				console.log(newItem)
+				news.appendChild(newItem)
+			}, 5000);
+			// newItem.innerHTML = image + h3
+			// console.log(newItem)
+			// news.appendChild(newItem)
+		});
 
-closeBtn.addEventListener('click', (e) => {
-    document.querySelector('.links').classList.remove('links__visibility');
-    bar.style.display = 'block';
-    closeBtn.style.display = 'none'
-})
-
-document.querySelector('a').addEventListener('click', e => {
-    console.log('hello');
-})
-
-document.querySelector('.services__collab').addEventListener('click', e => {
-    document.querySelector('.services__card').classList.toggle('services__vis')
-})
-
+		// Trigger animation when element is added
+		setTimeout(() => {
+			news.classList.add('slide-in');
+		}, 5000);
+	})
+	.catch(error => {
+	console.error('Error fetching news:', error);
+});
