6f96598b1c82e639699b36c33c27145f91885cb1
edgararthur <gilfoyle.arthur@protonmail.com>
2024-12-24 12:30:11 +0000

    changed font size of mission statement

--- src/components/about/Mission.tsx
+++ src/components/about/Mission.tsx
@@ -9,13 +9,13 @@ export default function Mission() {
         The Linguistics Association of Ghana aims to advance scholarship concerning the role of language in society as it relates to group and individual behaviour. The purposes of LAG are:
         <br />
         <br />
-        <h5 className='text-lg text-gray-800 font-medium'><span>1.</span> Provide an international forum to foster the growth and development of scholarly research in the area of language and social psychology;</h5>
+        <h5 className='text-sm text-gray-800 font-medium'><span>1.</span> Provide an international forum to foster the growth and development of scholarly research in the area of language and social psychology;</h5>
         <br />
-        <h5 className='text-lg text-gray-800 font-medium'><span>2.</span> Provide opportunities for our members to meet regularly in order to share and develop socially significant research;</h5>
+        <h5 className='text-sm text-gray-800 font-medium'><span>2.</span> Provide opportunities for our members to meet regularly in order to share and develop socially significant research;</h5>
         <br />
-        <h5 className='text-lg text-gray-800 font-medium'><span>3.</span> Promote scholarship that addresses pressing issues of our time as well as historically rooted social issues; and</h5>
+        <h5 className='text-sm text-gray-800 font-medium'><span>3.</span> Promote scholarship that addresses pressing issues of our time as well as historically rooted social issues; and</h5>
         <br />
-        <h5 className='text-lg text-gray-800 font-medium'><span>4.</span> Facilitate opportunities to publish, present, and promote language and social psychology research.</h5>
+        <h5 className='text-sm text-gray-800 font-medium'><span>4.</span> Facilitate opportunities to publish, present, and promote language and social psychology research.</h5>
         <br />
       </p>
     </Card>
